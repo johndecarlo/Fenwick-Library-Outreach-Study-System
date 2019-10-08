186b88e1db899c29f8d04fb4bd8e11dd64ac8eaf
John DeCarlo <jdecarlo841@gmail.com>
2019-10-08 10:42:12 -0400

    Basic Design Implementation
    Updated the layout implementation for the GUI and added the JButton Implementation so that maps will be displayed for different levels

--- FLOSSDriver.java
+++ FLOSSDriver.java
@@ -15,6 +15,14 @@ import javax.swing.*;
 public class FLOSSDriver {
 
    public static StudySystemMap fenwickLibrary;
+   public static JPanel buttons;
+   public static JPanel sidePanel;
+   
+   
+   public static JPanel tableInformationPanel;
+   public static JPanel searchPanel;
+   public static JPanel profileInformationPanel;
+   
    public static JButton floor1;
    public static JButton floor2;
    public static JButton floor3;
@@ -23,33 +31,132 @@ public class FLOSSDriver {
    
    public static void main(String[]args) throws IOException {
       JFrame display = new JFrame("FLOSS");     //Create our JFrame
-      fenwickLibrary = new StudySystemMap();    //Initialize the shot fenwick library map
-      displayButtons();                         //Add display option buttons
       display.setSize(1000, 850);			      //Size of display window
       display.setLocation(300, 0);				   //Location of display window on the screen
       display.setResizable(false);              //Cannot change size of the screen
       display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //Exit when close out 
-      display.setContentPane(fenwickLibrary);                    //Set contentPanel to our board
-      display.setVisible(true);                                //Make the screen visible
+      
+      JPanel container = new JPanel();          //Establish the JPanel that will hold all our JPanel's
+      container.setLayout(null);                //Set the layout to null so we can set the bounds
+   
+      fenwickLibrary = new StudySystemMap();    //Initialize the interactive Fenwick library map
+      fenwickLibrary.setBounds(0, 0, 700, 700);  //Set the bounds for the interactive map
+      buttons = new JPanel();                      //Initialize the buttons listing all the options
+      buttons.setBounds(0, 700, 700, 120);         //Set the bounds for the interactive map
+      displayButtons();                            //Add all the buttons and options
+      sidePanel = new JPanel();                    //Initialize the side panel that shows all our options
+      sidePanel.setBounds(700, 0, 140, 120);       //Set the bounds for the interactive map
+      initializeSizePanels();
+      loadSidePanel(1);
+   
+      container.add(fenwickLibrary);
+      container.add(buttons);
+      container.add(sidePanel);
+      
+      display.setContentPane(container);
+      display.setVisible(true);
    }
    
    public static void displayButtons()
    {
-      fenwickLibrary.setLayout(null);
-      JButton floor1 = new JButton("1st Floor");
-      JButton floor2 = new JButton("2nd Floor");
-      JButton floor3 = new JButton("3rd Floor");
-      JButton search = new JButton("Search");
-      JButton profile = new JButton("My Profile");
-      floor1.setBounds(0, 700, 140, 100);
-      floor2.setBounds(140, 700, 140, 100);
-      floor3.setBounds(280, 700, 140, 100);
-      search.setBounds(420, 700, 140, 100);
-      profile.setBounds(560, 700, 140, 100);
-      fenwickLibrary.add(floor1);
-      fenwickLibrary.add(floor2);
-      fenwickLibrary.add(floor3);
-      fenwickLibrary.add(search);
-      fenwickLibrary.add(profile); 
+      buttons.setLayout(null);
+      floor1 = new JButton("1st Floor");   //Initialize the JButton
+      floor2 = new JButton("2nd Floor");   //Initialize the JButton
+      floor3 = new JButton("3rd Floor");   //Initialize the JButton
+      ImageIcon searchIcon = new ImageIcon("images/search_icon.png");    //Initialize the image to add to the JButton
+      ImageIcon profileIcon = new ImageIcon("images/profile_icon.png");  //Initialize the image to add to the JButton
+      search = new JButton(searchIcon);    //Add button to the JFrame
+      profile = new JButton(profileIcon);  //Add button to the JFrame
+      floor1.setBounds(0, 0, 140, 120);       //Set the bounds to the floor 1 button
+      floor2.setBounds(140, 0, 140, 120);     //Set the bounds to the floor 2 button
+      floor3.setBounds(280, 0, 140, 120);     //Set the bounds to the floor 3 button
+      search.setBounds(420, 0, 140, 120);     //Set the bounds to the search button
+      profile.setBounds(560, 0, 140, 120);    //Set the bounds to the profile button
+      floor1.addActionListener(
+         new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               enableButtons();
+               floor1.setEnabled(false);
+               loadSidePanel(1);
+               fenwickLibrary.setFloorNumber(1);
+               fenwickLibrary.repaint();
+            } });   
+      floor2.addActionListener(
+         new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               enableButtons();
+               floor2.setEnabled(false);
+               loadSidePanel(2);
+               fenwickLibrary.setFloorNumber(2);
+               fenwickLibrary.repaint();
+            } });    
+      floor3.addActionListener(
+         new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               enableButtons();
+               floor3.setEnabled(false);
+               loadSidePanel(3);
+               fenwickLibrary.setFloorNumber(3);
+               fenwickLibrary.repaint();
+            } });  
+      search.addActionListener(
+         new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               enableButtons();
+               search.setEnabled(false);
+               loadSidePanel(4);
+            } });  
+      profile.addActionListener(
+         new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               enableButtons();
+               profile.setEnabled(false);
+               loadSidePanel(5);
+            } });  
+      buttons.add(floor1);      //Add button to the JFrame
+      buttons.add(floor2);      //Add button to the JFrame
+      buttons.add(floor3);      //Add button to the JFrame
+      buttons.add(search);      //Add button to the JFrame
+      buttons.add(profile);     //Add button to the JFrame
    }
+   
+   public static void loadSidePanel(int option) {
+      if(option == 1 || option == 2 || option == 3) {
+         sidePanel = tableInformationPanel;
+      }
+      else if(option == 4) {
+         sidePanel = searchPanel;
+      }
+      else {
+         sidePanel = profileInformationPanel;
+      }
+   }
+   
+   public static void enableButtons() {
+      floor1.setEnabled(true);
+      floor2.setEnabled(true);
+      floor3.setEnabled(true);
+      search.setEnabled(true);
+      profile.setEnabled(true);
+   }
+   
+   public static void initializeSizePanels() {
+      initializeInfoPanel();
+      initializeSearchPanel();
+      initializeProfilePanel();
+   }
+   
+   public static void initializeInfoPanel() {
+      tableInformationPanel = new JPanel();
+   }
+   
+   public static void initializeSearchPanel() {
+      searchPanel = new JPanel();
+   }
+   
+   public static void initializeProfilePanel() {
+      profileInformationPanel = new JPanel();
+   }
+   
+   
 }
--- StudySystemMap.java
+++ StudySystemMap.java
@@ -16,18 +16,34 @@ import java.io.*;
 public class StudySystemMap extends JPanel {
 
    private ImageIcon floor1 = new ImageIcon("Fenwick_Library_Maps/Floor1.png");
+   private ImageIcon floor2 = new ImageIcon("Fenwick_Library_Maps/Floor2.png");
+   private ImageIcon floor3 = new ImageIcon("Fenwick_Library_Maps/Floor3.png");
+   private ImageIcon searchIcon = new ImageIcon("search_icon.png");
+   private ImageIcon profileIcon = new ImageIcon("profile_icon.png");
+   private static int floorNumber;
 
    public StudySystemMap() {
+      floorNumber = 1;
+   }
    
+   public void setFloorNumber(int num) {
+      this.floorNumber = num;
    }
    
    public void paintComponent(Graphics g) {
       g.setColor(Color.red);     //Set the background color to no pieces as red
       super.paintComponent(g); 	//Call super method
-      paintBoard(g);
+      paintBoard(g, floorNumber);
    }
    
-   public void paintBoard(Graphics g) {
-      g.drawImage(floor1.getImage(), 0, 0, 700, 700, null);
+   public void paintBoard(Graphics g, int floor) {
+      if(floor == 1)
+         g.drawImage(floor1.getImage(), 0, 0, 700, 700, null);
+      else if(floor == 2)
+         g.drawImage(floor2.getImage(), 0, 0, 700, 700, null);
+      else if(floor == 3)
+         g.drawImage(floor3.getImage(), 0, 0, 700, 700, null);
+      else
+         g.drawImage(floor1.getImage(), 0, 0, 700, 700, null);
    }
 }
