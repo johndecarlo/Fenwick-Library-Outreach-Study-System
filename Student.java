46c92dcfe60107e7dd100da4961172129df1fca3
Jin-210 <55119553+Jin-210@users.noreply.github.com>
2019-11-01 20:14:15 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,70 +3,76 @@ import java.util.*;
 
 public class Student {
 	
+	//Student Fields
 	private String FirstName, LastName, password;
-	private int G_number, numberOfFriends;
-	private ArrayList<friend> friends;
+	private int G_number, numberOfFriends = 0;
+	private ArrayList<Student> friends;
 	
 	
+	//Scanner to read user's input
 	Scanner input = new Scanner(System.in);
 	
 	
-	
+	//Student Constructor, create a default one.
 	public Student() {
-		System.out.println("Thank you for creating an account.");
-		System.out.println("Please enter your your first name: ");
-		System.out.println("Please enter your last name: ");
-		System.out.println("Please enter your password");
-		this.FirstName = input.next();
-		this.LastName = input.next();
-		this.password = input.next();
 	}
 	
-	
+	//Setter for FirstName
 	public void setFirstName(String firstName) {
 		this.FirstName = firstName;
 	}
 	
+	//Getter for FirsttName
 	public String getFirstName() {
 		return FirstName;
 	}
 	
+	//Setter for LastName
 	public void setLastName(String lastName) {
 		this.LastName = lastName;
 	}
 	
+	//Getter for LastName
 	public String getLastName() {
 		return LastName;
 	}
 	
-	
+	//Setter for Gnumber
 	public void setG_number(int G_number) {
 		this.G_number = G_number;
 	}
 	
+	//Getter for Gnumber
 	public int get_Gnumber() {
 		return G_number;
 	}
 	
-	public void addFriends(friend friend) {
+	//Add friend into friend list, and increase number of friend by one
+	public void addFriends(Student friend) {
 		friends.add(friend);
 		numberOfFriends++;
 	}
 	
-	public void removeFriends(friend friend) {
+	//Remove friend from friends list, and decrease number of friend by one
+	public void removeFriends(Student friend) {
 		if(!friends.contains(friend)) {
 			System.out.println("The person is not in your friendList");
 		}else {
 			for(int i = 0; i < friends.size(); i++) {
-				
+				if(friends.get(i).equals(friend)) {
+					friends.remove(i);
+					numberOfFriends--;
+				}
 			}
 		}
 	}
 	
+	//Get number of friends the person have.
 	public int getNumberOfFriends() {
 		return numberOfFriends;
 	}
 	
+	//String output for class.
 	public String toString() {
 		String output = "First Name: " + FirstName + "\nLast Name: " + LastName + "\nG_number: " + G_number;
 		return output;
