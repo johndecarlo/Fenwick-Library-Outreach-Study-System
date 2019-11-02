d4a38a2bd44786aea2350751dcd5d1b37ca46674
Jin-210 <55119553+Jin-210@users.noreply.github.com>
2019-11-01 20:14:51 -0400

    Update Group.java

--- Group.java
+++ Group.java
@@ -2,25 +2,29 @@ import java.util.*;
 
 public class Group {
 	
-	//Limit of arraylist?
+	//Group fields
 	ArrayList<Student> groupList;
 	
 	public Group() {
 		
 	}
 	
+	//Class constructor with limit of 10
 	public void creatGroup() {
-		groupList = new ArrayList<Student>();
+		groupList = new ArrayList<Student>(10);
 	}
 	
+	//Delete group
 	public void deleteGroup() {
 		groupList = null;
 	}
 	
+	//Remove all members from group
 	public void clearGroup() {
 		groupList.clear();
 	}
 	
+	//Add member into a group. If group is null, 
 	public void addMember(Student member) {
 		if(groupList.equals(null)) {
 			System.out.println("Need to create group first");
@@ -29,13 +33,14 @@ public class Group {
 		}
 	}
 	
-	
+	//Check if person is in group return true or false if person is in group or not.
 	public boolean isMemberInGroup(Student member) {
 		boolean inGroup = false;
 		inGroup = groupList.contains(member);
 		return inGroup;
 	}
 	
+	//Remove the member from group. 
 	public void removeMember(Student member) {
 		for(int i = 0; i < groupList.size(); i++ ) {
 			if(groupList.get(i).equals(member)) {
